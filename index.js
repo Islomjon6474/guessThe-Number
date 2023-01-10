fc54f99f00d6e848e20640e2b4c378384f690f51
Islomjon6474 <islomjonrahimjonov6474@gmail.com>
2023-01-10 14:26:04 +0500

    first

---
+++ index.js
@@ -0,0 +1,29 @@
+let result_input = document.querySelector("#result_input");
+let guessBtn = document.querySelector("#guessBtn");
+let feedback = document.querySelector("#feedback");
+let test = document.querySelector("#test");
+let reload = document.querySelector("#reload");
+
+reload.addEventListener("click", () => {
+  window.location.reload();
+});
+
+let randomNum = Math.floor(Math.random() * 100) + 1;
+
+let attempts = 0;
+
+guessBtn.addEventListener("click", () => {
+  if (!result_input.value) {
+    feedback.innerHTML = "Please fill in the input above!";
+  } else if (result_input.value == randomNum) {
+    attempts++;
+    feedback.innerHTML =
+      "Congratulations, you win! The number of attempt is " + attempts;
+  } else if (result_input.value > randomNum) {
+    feedback.innerHTML = "Too high.";
+    attempts++;
+  } else if (result_input.value < randomNum) {
+    feedback.innerHTML = "Too low.";
+    attempts++;
+  }
+});
